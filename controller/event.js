6dd8b2c12f22570744d5c964b5b7702e4a56a02f
Thareq Muhammad Azani <thareqazani@gmail.com>
2023-09-27 15:15:37 +0700

    add event creation

--- controller/event.js
+++ controller/event.js
@@ -1,6 +1,27 @@
-const { Event } = require("../models");
+const { Events } = require("../models");
 
 exports.handleGetEvents = async (req, res) => {
-  const events = await Event.findAll();
+  const events = await Events.findAll();
   res.json({ ok: true, data: events });
+};
+
+exports.handleEventCreation = async (req, res) => {
+  const { name, description, date, time, venue, city, poster, category } =
+    req.body;
+  try {
+    const event = await Events.create({
+      name,
+      description,
+      date,
+      time,
+      venue,
+      city,
+      poster,
+      category,
+    });
+
+    res.json({ ok: true, data: event });
+  } catch (error) {
+    res.status(500).json({ ok: false, message: error.message });
+  }
 };
--- routes/event.js
+++ routes/event.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 const eventController = require("../controller/event");
 
 router.get("/", eventController.handleGetEvents);
+router.post("/eventcreation", eventController.handleEventCreation);
 
 module.exports = router;
