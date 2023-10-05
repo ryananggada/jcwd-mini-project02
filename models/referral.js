3fd05f1181855b03eefeca97b4f1295f857d80bf
H4zardZ1 <101616102+H4zardZ1@users.noreply.github.com>
2023-10-05 12:34:20 +0700

    add association to referral code

--- models/referral.js
+++ models/referral.js
@@ -2,7 +2,9 @@
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Referral extends Model {
-    static associate(models) {}
+    static associate(models) {
+      Referral.hasOne(models.User, { foreignKey: "referralId" })
+    }
   }
   Referral.init(
     {
